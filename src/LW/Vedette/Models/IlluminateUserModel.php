74a116561cf0489c2510166808def6a8a3d2e39c
Bradley Weston <brad@legalweb.org.uk>
2014-10-03 08:59:30 +0100

    Illuminate user model implementation.

---
+++ src/LW/Vedette/Models/IlluminateUserModel.php
@@ -0,0 +1,8 @@
+<?php namespace LW\Vedette\Models;
+
+use Illuminate\Auth\UserInterface;
+use Illuminate\Database\Eloquent\Model;
+
+class IlluminateUserModel extends Model implements UserInterface, UserModelInterface {
+
+}
