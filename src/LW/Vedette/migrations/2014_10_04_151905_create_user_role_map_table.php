ac978e9b383d69607d634b4fae2ff08518fd6cfc
Bradley Weston <brad@legalweb.org.uk>
2014-10-04 15:31:44 +0100

    User map to role migration.

---
+++ src/LW/Vedette/migrations/2014_10_04_151905_create_user_role_map_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserRoleMapTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('user_role', function(Blueprint $table)
+		{
+			$table->increments('id');
+
+			$table->unsignedInteger('user_id');
+			$table->unsignedInteger('role_id');
+
+			$table->timestamps();
+
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->foreign('role_id')->references('id')->on('roles');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('user_role');
+	}
+
+}
