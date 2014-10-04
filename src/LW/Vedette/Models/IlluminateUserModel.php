ec0b22cc8932035dcd73c34f59f708c66770b0ce
Bradley Weston <brad@legalweb.org.uk>
2014-10-04 15:16:13 +0100

    Illuminate user model to inherit Illuminate requirements.

--- src/LW/Vedette/Models/IlluminateUserModel.php
+++ src/LW/Vedette/Models/IlluminateUserModel.php
@@ -1,8 +1,19 @@
 <?php namespace LW\Vedette\Models;
 
+use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Reminders\RemindableTrait;
 
 class IlluminateUserModel extends Model implements UserInterface, UserModelInterface {
+
+	use UserTrait, RemindableTrait;
+
+	/**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'users';
 
 }
