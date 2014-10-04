b919aa77a600159dd4d817afec65320adf6ea33c
Bradley Weston <brad@legalweb.org.uk>
2014-10-04 15:31:29 +0100

    Permission table migration.

---
+++ src/LW/Vedette/migrations/2014_10_04_151904_create_permissions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePermissionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('permissions', function(Blueprint $table)
+		{
+			$table->string('id')->unique();
+
+			$table->string('label');
+
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('permissions');
+	}
+
+}
