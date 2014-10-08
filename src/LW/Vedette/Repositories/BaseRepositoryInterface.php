87edd2928e293da3d5fba6a9c2ea85e85cd426c9
Bradley Weston <brad@legalweb.org.uk>
2014-10-08 05:03:45 +0100

    Repositories should have methods to find and to get by criteria.

--- src/LW/Vedette/Repositories/BaseRepositoryInterface.php
+++ src/LW/Vedette/Repositories/BaseRepositoryInterface.php
@@ -1,5 +1,17 @@
 <?php namespace LW\Vedette\Repositories;
 
 interface BaseRepositoryInterface {
-	
+
+	/**
+	 * @param \LW\Vedette\Repositories\Criteria\CriteriaInterface $criteria
+	 * @return \LW\Vedette\Models\BaseModelInterface[]
+	 */
+	public function getByCrtieria(Crtieria\CrtieriaInterface $criteria);
+
+	/**
+	 * @param \LW\Vedette\Repositories\Criteria\CriteriaInterface $criteria
+	 * @return \LW\Vedette\Models\BaseModelInterface
+	 */
+	public function findByCrtieria(Crtieria\CrtieriaInterface $criteria);
+
 }
